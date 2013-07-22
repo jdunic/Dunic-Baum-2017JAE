1f32fe3314fdfe04c0ca2930e7886e4d203cd3fa
J Dunic <jdunic+github@gmail.com>
2013-07-22 15:41:53 -0700

    setting up windows (for plotting)

---
+++ Quartz_setup.R
@@ -0,0 +1,18 @@
+# REPL workflow setup
+# First quartz window = quartz 2
+quartz()
+par(mfrow = c(1, 1))
+A2 <- par(mfrow = c(1, 1))
+par(mfrow = c(2, 1))
+B2 <- par(mfrow = c(2, 1))
+par(mfrow = c(3, 2))
+C2 <- par(mfrow = c(3, 2))
+
+# Second quartz window = quartz 3
+quartz()
+par(mfrow = c(1, 1))
+A3 <- par(mfrow = c(1, 1))
+par(mfrow = c(2, 1))
+B3 <- par(mfrow = c(2, 1))
+par(mfrow = c(3, 2))
+C3 <- par(mfrow = c(3, 2))
